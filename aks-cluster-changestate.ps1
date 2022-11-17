41f862ce17d0925773b08ff7460f2d694235ed38
Gordon Byers <gordon.byers@microsoft.com>
2022-11-17 14:20:10 +0000

    Shifting to use PsWorkflow

--- aks-cluster-changestate.ps1
+++ aks-cluster-changestate.ps1
@@ -1,46 +1,48 @@
-Import-module Az.Aks
+workflow aks-cluster-changestate {
 
-param(
-  [Parameter(Mandatory=$true)]
-  [String] $ResourceGroupName,
-
-  [Parameter(Mandatory=$true)]
-  [String] $AksClusterName,
-
-  [Parameter(Mandatory=$true)]
-  [ValidateSet('start','stop')]
-  [String] $Operation
-)
-
-try
-{
-  Disable-AzContextAutosave -Scope Process
-    
-  #System Managed Identity
-  $AzureContext = (Connect-AzAccount -Identity).context
-  $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
-}
-catch {
-  Write-Error -Message $_.Exception
-  throw $_.Exception
-}
-
-switch -Exact ($Operation)
-{
-    'start'
-    {
-      Write-Output "Starting Cluster $AksClusterName in $ResourceGroupName"
-      Start-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
-    }
-    'stop'
-    {
-      Write-Output "Stopping Cluster $AksClusterName in $ResourceGroupName"
-      Stop-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
-    }
-	# 'toggle'
-	# {
-	# 	Write-Output "Toggling Cluster State of $AksClusterName in $ResourceGroupName"
-	# 	Get-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName | select -ExpandProperty ProvisioningState
-	# 	Start-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
-	# }
+	Param
+	(
+		[Parameter(Mandatory=$true)]
+		[String] $ResourceGroupName,
+		
+		[Parameter(Mandatory=$true)]
+		[String] $AksClusterName,
+		
+		[Parameter(Mandatory=$true)]
+		[ValidateSet('start','stop')]
+		[String] $Operation
+	)
+	
+	try
+	{
+		Disable-AzContextAutosave -Scope Process
+			
+		#System Managed Identity
+		$AzureContext = (Connect-AzAccount -Identity).context
+		$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+	}
+	catch {
+		Write-Error -Message $_.Exception
+		throw $_.Exception
+	}
+	
+	switch -CaseSensitive ($Operation)
+	{
+		'start'
+		{
+		Write-Output "Starting Cluster $AksClusterName in $ResourceGroupName"
+		Start-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
+		}
+		'stop'
+		{
+		Write-Output "Stopping Cluster $AksClusterName in $ResourceGroupName"
+		Stop-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
+		}
+		# 'toggle'
+		# {
+		# 	Write-Output "Toggling Cluster State of $AksClusterName in $ResourceGroupName"
+		# 	Get-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName | select -ExpandProperty ProvisioningState
+		# 	Start-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
+		# }
+	}
 }
