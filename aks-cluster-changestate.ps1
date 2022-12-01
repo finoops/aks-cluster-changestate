ecf0f3b469c66226cc06c58be0b2dbfe95cd8706
Gordon Byers <gordon.byers@microsoft.com>
2022-12-01 09:34:28 +0000

    Update aks-cluster-changestate.ps1

--- aks-cluster-changestate.ps1
+++ aks-cluster-changestate.ps1
@@ -1,48 +1,69 @@
-Param
-(
-	[Parameter(Mandatory=$true)]
+<#
+    .SYNOPSIS
+        This Azure Automation runbook automates the scheduled shutdown and startup of AKS Clusters in an Azure subscription. 
+
+    .DESCRIPTION
+        This is a PowerShell runbook, as opposed to a PowerShell Workflow runbook.
+		Note that the Automation Account will need RBAC permission on the Cluster (scoped directly or inherited) in order to
+		perform the start/stop operation.
+
+    .PARAMETER ResourceGroupName
+        The name of the ResourceGroup where the AKS Cluster is located
+    
+    .PARAMETER AzureSubscriptionName
+        The name of the AKS Cluster to
+    
+    .PARAMETER Operation
+        Currently supported operations are 'start' and 'stop'
+    
+    .INPUTS
+        None.
+
+    .OUTPUTS
+        Human-readable informational and error messages produced during the job. Not intended to be consumed by another runbook.
+#>
+
+Param(
+    [parameter(Mandatory=$true)]
 	[String] $ResourceGroupName,
-
-	[Parameter(Mandatory=$true)]
+    [parameter(Mandatory=$true)]
 	[String] $AksClusterName,
-
-	[Parameter(Mandatory=$true)]
+    [parameter(Mandatory=$true)]
 	[ValidateSet('start','stop')]
-	[String] $Operation
+    [String]$Operation
 )
+	
+try
+{
+	Disable-AzContextAutosave -Scope Process
+		
+	#System Managed Identity
+	Write-Output "Logging into Azure using System Managed Identity"
+	$AzureContext = (Connect-AzAccount -Identity).context
+	$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+}
+catch {
+	Write-Error -Message $_.Exception
+	throw $_.Exception
+}
 
-workflow aks-cluster-changestate {
-	
-	try
+Write-Output "Performing $Operation"
+switch -CaseSensitive ($Operation)
+{
+	'start'
 	{
-		Disable-AzContextAutosave -Scope Process
-			
-		#System Managed Identity
-		$AzureContext = (Connect-AzAccount -Identity).context
-		$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+	Write-Output "Starting Cluster $AksClusterName in $ResourceGroupName"
+	Start-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
 	}
-	catch {
-		Write-Error -Message $_.Exception
-		throw $_.Exception
-	}
-	
-	switch -CaseSensitive ($Operation)
+	'stop'
 	{
-		'start'
-		{
-		Write-Output "Starting Cluster $AksClusterName in $ResourceGroupName"
-		Start-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
-		}
-		'stop'
-		{
-		Write-Output "Stopping Cluster $AksClusterName in $ResourceGroupName"
-		Stop-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
-		}
-		# 'toggle'
-		# {
-		# 	Write-Output "Toggling Cluster State of $AksClusterName in $ResourceGroupName"
-		# 	Get-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName | select -ExpandProperty ProvisioningState
-		# 	Start-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
-		# }
+	Write-Output "Stopping Cluster $AksClusterName in $ResourceGroupName"
+	Stop-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
 	}
+	# 'toggle'
+	# {
+	# 	Write-Output "Toggling Cluster State of $AksClusterName in $ResourceGroupName"
+	# 	Get-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName | select -ExpandProperty ProvisioningState
+	# 	Start-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AksClusterName
+	# }
 }
